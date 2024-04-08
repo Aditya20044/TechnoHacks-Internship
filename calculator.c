7ef4de12830f09d572d520ffcdbb12ccf972c87e
Aditya20044 <118764498+Aditya20044@users.noreply.github.com>
2024-04-08 13:29:41 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double firstNumber, secondNumber;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+    printf("Enter the first number: ");
+    scanf("%lf", &firstNumber);
+    printf("Enter the second number: ");
+    scanf("%lf", &secondNumber);
+
+    switch (operator) {
+        case '+':
+            printf("%.1lf + %.1lf = %.1lf\n", firstNumber, secondNumber, firstNumber + secondNumber);
+            break;
+        case '-':
+            printf("%.1lf - %.1lf = %.1lf\n", firstNumber, secondNumber, firstNumber - secondNumber);
+            break;
+        case '*':
+            printf("%.1lf * %.1lf = %.1lf\n", firstNumber, secondNumber, firstNumber * secondNumber);
+            break;
+        case '/':
+            if(secondNumber != 0.0)
+                printf("%.1lf / %.1lf = %.1lf\n", firstNumber, secondNumber, firstNumber / secondNumber);
+            else
+                printf("Divide by zero situation.\n");
+            break;
+        default:
+            printf("Error! Operator is not correct\n");
+    }
+
+    return 0;
+}
+
